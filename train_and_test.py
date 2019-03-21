26e8aa6fa7503b9a0ffa8d4c17f26b993f0b71df
Henry Rice <henry3241@gmail.com>
2019-03-21 12:02:03 -0500

    starting to implement training

---
+++ train_and_test.py
@@ -0,0 +1,18 @@
+# functions used to process training data and make predictions about testing data
+
+import utils
+
+#returns the mean and standard deviation of all 3 distributions
+#distributions['C'['mean']]
+#distributions['H'['std_dev']]
+def train(fasta_train):
+    return None
+
+def main():
+    #get filenames
+    fasta, ss = utils.parse_args()
+    #split data into training and testing sets
+    fasta_train, fasta_test = utils.split_files(fasta, ss)
+    #train, creating 3 Gaussian distributions, 1 for each possible SS label
+    #each distribution will be stored as pairs of mean and standard deviation
+    distributions = train(fasta_train)
