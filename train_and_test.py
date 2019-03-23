38035e3fd7818a2bb147198a338e5c53ec84bdff
HenryRobertRice <henry3241@gmail.com>
2019-03-23 15:14:33 -0500

    deleted some old implementation and changed fasta to pssm

--- train_and_test.py
+++ train_and_test.py
@@ -2,19 +2,8 @@
 
 import utils
 
-
-# returns the mean and standard deviation of all 3 distributions
-# distributions['C']['mean'] = mean of distribution representing P(Xi | Y = C)
-# distributions['H']['std_dev'] = standard deviation of distribution representing P(Xi | Y = H)
-def train(fasta_train):
-    return None
-
-
 def main():
     # get filenames
-    fasta, ss = utils.parse_args()
+    pssm_list, ss_list, pssm_dir, ss_dir = utils.parse_args()
     # split data into training and testing sets
-    fasta_train, fasta_test = utils.split_files(fasta, ss)
-    # train, creating 3 Gaussian distributions, 1 for each possible SS label
-    # each distribution will be stored as pairs of mean and standard deviation
-    distributions = train(fasta_train)
+    pssm_train, pssm_test = utils.split_files(pssm, ss)
